7553a10e8532452d36961817fc099c974431067f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-07-17 16:27:18 +0900

    <update>ポジションを指定するのではなく色を変えるようにした

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class KeyController : ItemData
+{
+    public override void Active()
+    {
+        Debug.Log("Œ®‚ðƒQƒbƒg");
+        _spriteRenderer.color = Color.white;
+    }
+}
